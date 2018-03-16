47e6fa20d573e7035657d1a7bd78a3b4c4e3b6c7
Ashwin Nair <ashwinnair2610@gmail.com>
2018-03-16 18:12:21 +0530

    Added allocation

--- foodalloc/homepage/views.py
+++ foodalloc/homepage/views.py
@@ -6,6 +6,8 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import FooDB
 
+from random import randint, sample
+
 # Create your views here.
 
 
@@ -27,25 +29,40 @@ class SetAlternativesView(LoginRequiredMixin, TemplateView):
 def get_food_details(request):
 	if request.method == 'POST':
 		form = AllocateFoodWithPhysicalTraits(request.POST)
-		
+
 		if form.is_valid():
 			height, weight, age, gender = form.clean_food_alloc_data()
 
-		if gender == 'M':	
+		if gender == 'M':
 			bmr = 88.362 + (13.397 * weight) + (4.799 * height) - (5.677 * age)
 		else:
 			bmr = 447.593 + (9.247 * weight) + (3.098 * height) - (4.330 * age)
 
 		calories_required = bmr *1.55
 
-		print (bmr)
-		allocate(calories_required)
+		breakfast, lunch, dinner = allocate(calories_required)
 
-
-		# Have to do something here
+		return render(request, 'alloc/allocated_food.html', {'breakfast': breakfast, 'lunch': lunch, 'dinner': dinner})
 	else:
 		form = AllocateFoodWithPhysicalTraits()
 		return render(request, 'alloc/allocate_food_physical.html', {'form': form})
+
+
+def get_food_range(request):
+    if request.method == 'POST':
+        form = FoodInfo(request.POST)
+		# Have to do something here
+        if form.is_valid():
+            upper_bound = form.cleaned_data['upper_bound']
+            lower_bound = form.cleaned_data['lower_bound']
+            breakfast, lunch, dinner = allocate(randint(lower_bound, upper_bound))
+
+            return render(request, 'alloc/allocated_food.html', {'breakfast': breakfast, 'lunch': lunch, 'dinner': dinner})
+
+    else:
+        form = FoodInfo()
+        return render(request, 'alloc/allocate_food_physical.html', {'form': form})
+
 
 def get_food_info(request):
 	if request.method == 'POST':
@@ -67,7 +84,7 @@ def get_food_name(request):
 		if form.is_valid():
 			name = form.cleaned_data['name']
 			items = FooDB.objects.filter(food__iexact=name)
-			return render(request, 'alloc/result.html', {'items': items})	
+			return render(request, 'alloc/result.html', {'items': items})
 
 	else:
 		form = LookupFoodWithFoodName()
@@ -76,5 +93,44 @@ def get_food_name(request):
 
 
 
+
 def allocate(calories):
-	pass #define function here function here
+    type1 = (FooDB.objects.filter(food_type = 'Fruit') | FooDB.objects.filter(food_type = 'Nut')).order_by('?')
+    type2 = (FooDB.objects.filter(food_type = 'Vegetable') | FooDB.objects.filter(food_type = 'Meat') | FooDB.objects.filter(food_type = 'Legumes')).order_by('?')
+
+    breakfast = []
+    lunch = []
+    dinner = []
+
+    breakfast_items = [0, 0, 0]
+    lunch_items = [0, 0, 0, 0, 0]
+    dinner_items = [0, 0, 0, 0, 0]
+
+
+    for i in range(3):
+        breakfast.append(type1[randint(0, len(type1)-1)])
+        breakfast_items[i] = [breakfast[i].food]
+        breakfast_items[i].append(("("+breakfast[i].food_type+")"))
+
+    for i in range(5):
+        lunch.append(type2[randint(0, len(type2)-1)])
+        lunch_items[i] = [lunch[i].food]
+        lunch_items[i].append(("("+lunch[i].food_type+")"))
+
+        dinner.append(type2[randint(0, len(type2)-1)])
+        dinner_items[i] = [dinner[i].food]
+        dinner_items[i].append(("("+dinner[i].food_type+")"))
+
+
+    for i in range(3):
+        quantity = (calories/9) * (100/breakfast[i].calories)
+        breakfast_items[i].append((str(int(quantity))+"g"))
+
+    for i in range(5):
+        quantity = (calories/15) * (100/lunch[i].calories)
+        lunch_items[i].append((str(int(quantity))+"g"))
+
+        quantity = (calories/15) * (100/dinner[i].calories)
+        dinner_items[i].append((str(int(quantity))+"g"))
+
+    return breakfast_items, lunch_items, dinner_items
